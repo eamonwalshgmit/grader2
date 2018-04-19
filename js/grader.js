ca41753060e10fbbc3eb49149f78dc2d582dbcce
eamonwalshgmit <eamon.walsh@gmit.ie>
2018-04-19 15:35:05 +0100

    Version 1 - just getting started

---
+++ js/grader.js
@@ -0,0 +1,67 @@
+function gradeResult() {
+
+  var myEnglishResult = parseInt(document.getElementById("value-english").value);
+  var myIrishResult = parseInt(document.getElementById("value-irish").value);
+  var myMathsResult = parseInt(document.getElementById("value-maths").value);
+  var englishGrade, irishGrade, mathsGrade;
+
+  if (!myEnglishResult)
+      alert("Please enter your English result");
+  else if (myEnglishResult >= 90)
+        englishGrade = 'A';
+  else if (myEnglishResult >= 80)
+        englishGrade = 'B';
+  else if (myEnglishResult >= 70)
+        englishGrade = 'B-';
+  else if (myEnglishResult >= 60)
+        englishGrade = 'C';
+  else if (myEnglishResult >= 50)
+        englishGrade = 'C-';
+  else if (myEnglishResult >= 40)
+        englishGrade = 'D';
+  else if (myEnglishResult < 40)
+        englishGrade = 'F';
+  
+  if (!myIrishResult)
+      alert("Please enter your English result");
+  else if (myIrishResult >= 90)
+        irishGrade = 'A';
+  else if (myIrishResult >= 80)
+        irishGrade = 'B';
+  else if (myIrishResult >= 70)
+        irishGrade = 'B-';
+  else if (myIrishResult >= 60)
+        irishGrade = 'C';
+  else if (myIrishResult >= 50)
+        irishGrade = 'C-';
+  else if (myIrishResult >= 40)
+        irishGrade = 'D';
+  else if (myIrishResult < 40)
+        irishGrade = 'F';
+
+  if (!myMathsResult)
+      alert("Please enter your Maths result");
+  else if (myMathsResult >= 90)
+        mathsGrade = 'A';
+  else if (myMathsResult >= 80)
+        mathsGrade = 'B';
+  else if (myMathsResult >= 70)
+        mathsGrade = 'B-';
+  else if (myMathsResult >= 60)
+        mathsGrade = 'C';
+  else if (myMathsResult >= 50)
+        mathsGrade = 'C-';
+  else if (myMathsResult >= 40)
+        mathsGrade = 'D';
+  else if (myMathsResult < 40)
+        mathsGrade = 'F';
+    
+    var message1 = 'You got a ' + englishGrade + ' in English!';
+    var message2 = 'You got a ' + irishGrade + ' in Irish!';
+    var message3 = 'You got a ' + mathsGrade + ' in Maths!';
+
+    document.getElementById("englishresultsentence").innerHTML = message1;
+    document.getElementById("irishresultsentence").innerHTML = message2;
+    document.getElementById("mathsresultsentence").innerHTML = message3;
+
+}
